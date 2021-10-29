a92ce81532498a8a44632296d41e0d46b30b8e19
Vasily V. Blinkov <vblinkov@fbsd.gf1>
2021-10-29 08:42:59 +0300

    Popovers

--- src/js/popovers.js
+++ src/js/popovers.js
@@ -3,3 +3,7 @@ var popoverTriggerList = Array.from(
   document.querySelectorAll('[data-bs-toggle="popover"]')
 );
 
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
+
